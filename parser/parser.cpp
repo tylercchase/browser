4fd51a6022f7fd89aa6e832732a4440a8626a763
Tyler Chase <tylercchase@gmail.com>
2021-03-17 00:54:10 -0800

    WIP: revamp parser

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,6 +1,10 @@
 #include "parser.h"
 #include <algorithm>
+<<<<<<< Updated upstream
 #include <iostream>
+=======
+#include <string>
+>>>>>>> Stashed changes
 #include <sstream>
 void readChildren(Node &x, int spacer) {
   for (int i = 0; i < spacer; ++i) {
@@ -14,39 +18,78 @@ void readChildren(Node &x, int spacer) {
   std::for_each(x.children.begin(), x.children.end(),
                 [spacer](Node &n) { readChildren(n, spacer + 1); });
 };
-void Parser::passHTML(std::istream &HTMLFile) {
-  std::string line;
-  Node *current;
-  while (std::getline(HTMLFile, line, '>')) {
-    Node a;
-    if (DOM.size() == 0) {
-      a.name = line.substr(1);
-      DOM.push_back(a);
-      current = &DOM[0];
-      continue;
-    }
-    if (line.find('/') == -1) {
-      a.text = line.substr(0, line.find('<'));
-      a.name = line.substr(line.find('<') + 1);
-      current->children.push_back(a);
-      std::stringstream attributes(line.substr(line.find('<') + 1));
-      std::string attr;
-      getline(attributes, attr, ' ');
-      while (getline(attributes, attr, ' ')) {
-        Param param;
-        param.name = attr.substr(0, attr.find('='));
-        param.value = attr.substr(attr.find('='));
-        a.params.push_back(param);
-      }
-      auto ref = current;
-      current = &current->children.back();
-      current->parent = ref;
-    } else {
-      auto content = line.substr(0, line.find('<'));
-      if (content.find_first_not_of(' ') != std::string::npos) {
-        current->text = content;
-      }
-      current = current->parent;
+void Parser::passHTML( std::istream &HTMLFile)
+{
+    std::string line;
+    Node * current;
+    while (std::getline(HTMLFile, line, '>'))
+    {
+        Node a;
+        if(DOM.size() == 0){
+            a.name = line.substr(1);
+            DOM.push_back(a);
+            current = &DOM[0];
+            continue;
+        }
+        if (line.find('/') == -1)
+        {
+            bool spaceFlag = false;
+            bool attrFlag = false;
+            bool quoteFlag = false;
+            Param param;
+            for(auto &character: line) {
+                if(!spaceFlag && std::isspace(character)) {
+                    spaceFlag = true;
+                }
+                if(spaceFlag) {
+                    if(!attrFlag && !std::isspace(character)) {
+                        if(character == '=') {
+                            attrFlag = true;
+                        }
+                        else {
+                            param.name += character;
+                        }
+                    } else if(attrFlag) {
+                        if(!quoteFlag && character == '"')
+                         {
+
+                        }
+                    }
+                    else {
+                        
+                    }
+                } else {
+                    a.name += character;
+                }
+            }
+            a.text = line.substr(0,line.find('<'));
+            a.name = line.substr(line.find('<')+1);
+            if(line.find(' ') != std::string::npos) {
+                a.name = line.substr(line.find('<')+1,line.find(' ')-2);
+            }
+            current->children.push_back(a);
+            std::stringstream attributes(line.substr(line.find('<')+1));
+            std::string attr;
+            getline(attributes,attr, ' ');
+            while(getline(attributes, attr, ' ')){
+                Param param;
+                param.name = attr.substr(0,attr.find('='));
+                param.value = attr.substr(attr.find('='));
+                a.params.push_back(param);
+            }
+            auto ref = current;
+            current = &current->children.back();
+            current->parent = ref;
+        }
+        else
+        {
+            auto content = line.substr(0,line.find('<'));
+            if(content.find_first_not_of(' ') != std::string::npos)
+            {
+                current->text = content;
+            }
+            current = current->parent;
+        }
     }
   }
   int spacer = 0;
--- tests/parser_test.cpp
+++ tests/parser_test.cpp
@@ -1,12 +1,23 @@
 #include "../parser/parser.h"
 #include <catch2/catch.hpp>
 #include <sstream>
+#include <iostream>
 
 TEST_CASE("1 is equal to 1") {
-  REQUIRE(1 == 1);
-  Parser parser;
-  std::istringstream asdf("<html>\n<body>\n<a href=\"index.html\" "
-                          "target=\"_blank\">Link to something</a>\n<p>Hello "
-                          "World</p>\n<span>YOLO</span>\n\n</body>\n</html>");
-  parser.passHTML(asdf);
+   REQUIRE(1 == 1);
+   Parser parser;
+   std::istringstream asdf("<html>\n<body>\n<a href=\"index.html\" target=\"_blank\">Link to something</a>\n<p>Hello World</p>\n<span>YOLO</span>\n\n</body>\n</html>");
+   parser.passHTML(asdf);
+   SECTION("Name parsing for basic html elements") {
+      REQUIRE(parser.DOM[0].name == "html");
+      REQUIRE(parser.DOM[0].children[0].name == "body");
+      REQUIRE(parser.DOM[0].children[0].children[0].name == "a");
+      REQUIRE(parser.DOM[0].children[0].children[1].name == "p");
+      REQUIRE(parser.DOM[0].children[0].children[2].name == "span");
+   }
+   SECTION("Attribute parsing") {
+      // REQUIRE(parser.DOM[0].children[0].children[0].params[0].name == "target");
+      // REQUIRE(parser.DOM[0].children[0].children[0].params[0].value == "value");
+      
+   }
 }
