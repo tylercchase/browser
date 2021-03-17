ec78956702907a862b7ab825a50770e313a948d0
Tyler Chase <tylercchase@gmail.com>
2021-03-17 01:20:00 -0800

    Element parsing

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,11 +1,8 @@
 #include "parser.h"
 #include <algorithm>
-<<<<<<< Updated upstream
 #include <iostream>
-=======
-#include <string>
->>>>>>> Stashed changes
 #include <sstream>
+#include <string>
 void readChildren(Node &x, int spacer) {
   for (int i = 0; i < spacer; ++i) {
     std::cout << " ";
@@ -18,78 +15,66 @@ void readChildren(Node &x, int spacer) {
   std::for_each(x.children.begin(), x.children.end(),
                 [spacer](Node &n) { readChildren(n, spacer + 1); });
 };
-void Parser::passHTML( std::istream &HTMLFile)
-{
-    std::string line;
-    Node * current;
-    while (std::getline(HTMLFile, line, '>'))
-    {
-        Node a;
-        if(DOM.size() == 0){
-            a.name = line.substr(1);
-            DOM.push_back(a);
-            current = &DOM[0];
-            continue;
+void Parser::passHTML(std::istream &HTMLFile) {
+  std::string line;
+  Node *current;
+  while (std::getline(HTMLFile, line, '>')) {
+    Node a;
+    if (DOM.size() == 0) {
+      a.name = line.substr(1);
+      DOM.push_back(a);
+      current = &DOM[0];
+      continue;
+    }
+    if (line.find('/') == -1) {
+      bool spaceFlag = false;
+      bool attrFlag = false;
+      bool quoteFlag = false;
+      Param param;
+      param.name = "";
+      param.value = "";
+      for (auto &character : line) {
+        if (!spaceFlag && (character == ' ')) {
+          spaceFlag = true;
         }
-        if (line.find('/') == -1)
-        {
-            bool spaceFlag = false;
-            bool attrFlag = false;
-            bool quoteFlag = false;
-            Param param;
-            for(auto &character: line) {
-                if(!spaceFlag && std::isspace(character)) {
-                    spaceFlag = true;
-                }
-                if(spaceFlag) {
-                    if(!attrFlag && !std::isspace(character)) {
-                        if(character == '=') {
-                            attrFlag = true;
-                        }
-                        else {
-                            param.name += character;
-                        }
-                    } else if(attrFlag) {
-                        if(!quoteFlag && character == '"')
-                         {
-
-                        }
-                    }
-                    else {
-                        
-                    }
-                } else {
-                    a.name += character;
-                }
+        if (spaceFlag) {
+          if (!attrFlag && !std::isspace(character)) {
+            if (character == '=') {
+              attrFlag = true;
+            } else {
+              param.name += character;
             }
-            a.text = line.substr(0,line.find('<'));
-            a.name = line.substr(line.find('<')+1);
-            if(line.find(' ') != std::string::npos) {
-                a.name = line.substr(line.find('<')+1,line.find(' ')-2);
+          } else if (attrFlag) {
+            if (!quoteFlag && character == '"') {
+              quoteFlag = true;
             }
-            current->children.push_back(a);
-            std::stringstream attributes(line.substr(line.find('<')+1));
-            std::string attr;
-            getline(attributes,attr, ' ');
-            while(getline(attributes, attr, ' ')){
-                Param param;
-                param.name = attr.substr(0,attr.find('='));
-                param.value = attr.substr(attr.find('='));
+            if (quoteFlag) {
+              if (character == '"') {
+                quoteFlag = false;
                 a.params.push_back(param);
+                param.name = "";
+                param.value = "";
+              }
+              param.value += character;
             }
-            auto ref = current;
-            current = &current->children.back();
-            current->parent = ref;
+          } else {
+          }
+        } else {
+          if (character != '<' && !std::isspace(character)) {
+            a.name += character;
+          }
         }
-        else
-        {
-            auto content = line.substr(0,line.find('<'));
-            if(content.find_first_not_of(' ') != std::string::npos)
-            {
-                current->text = content;
-            }
-            current = current->parent;
-        }
+      }
+      current->children.push_back(a);
+      auto ref = current;
+      current = &current->children.back();
+      current->parent = ref;
+    } else {
+      auto content = line.substr(0, line.find('<'));
+      if (content.find_first_not_of(' ') != std::string::npos) {
+        current->text = content;
+      }
+      current = current->parent;
     }
   }
   int spacer = 0;
